2f5fe941c7d20663fc90d10289ba18a5663314a3
jpepetrueno <dimagidve@gmail.com>
2024-10-02 18:43:38 -0400

    v.0.0.1

--- src/dimagid/arithmetic.clj
+++ src/dimagid/arithmetic.clj
@@ -1,46 +1,205 @@
 ;; ---------------------------------------------------------
 ;; dimagid.arithmetic
 ;;
-;; TODO: Provide a meaningful description of the project
+;; Description:
+;; -----------
+;; This library provides a collection of mathematical functions written in Clojure
+;; that offer arithmetic operations and properties for natural numbers.
+;;
+;; Goals:
+;; ------
+;; * Provide a simple and easy-to-use API for performing arithmetic operations in Clojure.
+;; * Offer a variety of functions for analyzing and manipulating natural numbers.
+;; * Be a useful tool for developers who need to perform mathematical calculations in their applications.
+;;
+;; Features:
+;; ----------
+;; * Check if a number is prime.
+;; * Find prime factors of a number.
+;; * Generate prime numbers up to a given limit.
+;; * Calculate arithmetic properties such as the sum of divisors and the perfection of a number.
+;; * Provide a word representation of a number.
+;; * Calculate the scale of a number (units, tens, hundreds, etc.).
+;;
+;; Usage:
+;; ------
+;; This library can be used in any Clojure project that requires arithmetic operations.
+;; The functions can be imported and used directly in the code.
+
 ;; ---------------------------------------------------------
 
 (ns dimagid.arithmetic
-  (:gen-class)
-  (:require 
-   [com.brunobonacci.mulog :as mulog]))
+  (:require
+   [clojure.pprint :as pp]
+   [clojure.math :as math]
+   [clojure.math.combinatorics :as combo]))
 
-;; ---------------------------------------------------------
-;; Application
+(defn prime?
+  "Check if the natural number `n` is prime.
 
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(defn greet
-  "Greeting message via Clojure CLI clojure.exec"
-  ([] (greet {:team-name "secret engineering"}))
-  ([{:keys [team-name]}]
-   (str "dimagid arithmetic service developed by the " team-name " team")))
+  Examples:
+  (prime? 5) ;; => true
+  (prime? 9) ;; => false
+  "
+  [n]
+  (.isProbablePrime (biginteger n) 9))
 
+(defn prime-factors
+  "Returns the prime factors of a given number `n`.
 
-(defn -main
-  "Entry point into the application via clojure.main -M"
-  [& args]
-  (let [team (first args)]
-    (mulog/set-global-context!
-     {:app-name "dimagid arithmetic" :version  "0.1.0-SNAPSHOT"})
-    (mulog/log ::application-starup :arguments args)
-    (if team
-      (println (greet team))
-      (println (greet)))))
+  Examples:
+  (prime-factors 12) ;; => [2 2 3]
+  (prime-factors 51) ;; => [3 17]
+  "
+  [n]
+  (loop [x n
+         factors []]
+    (if (< x 2)
+      factors
+      (if (prime? x)
+        (conj factors x)
+        (let [p (first (filter #(zero? (mod x %)) (cons 2 (range 3 (inc x) 2))))]
+          (recur (/ x p) (conj factors p)))))))
 
-;; ---------------------------------------------------------
+(defn primes
+  "Returns a vector of the first `n` prime numbers.
 
+  Example:
+  (primes 10) => [2 3 5 7 11 13 17 19 23 29]"
+  [n]
+  (case n
+    1 [2]
+    (let [x (* n 1.5)
+          limit (int (Math/ceil (* x (Math/log x))))
+          sieve (boolean-array limit true)
+          primes (atom [])
+          count-primes (atom 0)]
+      (loop [i 2]
+        (when (< @count-primes n)
+          (when (aget sieve i)
+            (swap! primes conj i)
+            (swap! count-primes inc)
+            (loop [j (* i i)]
+              (when (< j limit)
+                (aset sieve j false)
+                (recur (+ j i)))))
+          (if (= i 2)
+            (recur 3)
+            (recur (+ i 2)))))
+      @primes)))
 
-;; ---------------------------------------------------------
-;; Rick Comment
-#_{:clj-kondo/ignore [:redefined-var]}
-(comment
+(defn primes-up-to-n
+  "Returns a vector of prime numbers ≤ `n`, in ascending order.
 
-  (-main)
-  (-main {:team-name "Clojure Engineering"})
+  Example:
+  (primes-up-to-n 20) ;; => [2 3 5 7 11 13 17 19]
+  "
+  [n]
+  {:pre [(pos-int? n)]}
+  (into [] (filter #(<= % n)) (primes n)))
 
-  #_()) ; End of rich comment block
-;; ---------------------------------------------------------
+(defn nth-prime
+  "Given a positive integer `n`, returns the nth prime number.
+
+  Examples:
+  (nth-prime 1) ;; => 2
+  (nth-prime 6) ;; => 13
+  (nth-prime 1000000) ;; => 15485863
+  "
+  [n]
+  {:pre [(pos-int? n)]}
+  (if (= 1 n) 2 (last (primes n))))
+
+(def scales ["unit" "tens" "hundreds" "thousands" "ten thousands" "hundred thousands" "millions" "ten millions" "hundred millions" "billions" "ten billions" "hundred billions" "trillions"])
+
+(def zero-arithmetic-properties
+  {:is-even-or-odd? :even,
+   :perfect-square? false,
+   :spellout-number "zero",
+   :scale "zero",
+   :divisor-type nil,
+   :n 0,
+   :divisor-sum 0,
+   :divisors [],
+   :prime-factors [],
+   :perfect-cube? false,
+   :prime? false})
+
+(defn get-scale-name
+  "Returns the scale of a number `n` as a string.
+
+  Examples:
+  (get-scale-name 10)  ;; => \"tens\"
+  (get-scale-name 100) ;; => \"hundreds\"
+  "
+  [n]
+  (if (zero? n)
+    "zero"
+    (get scales (int (math/log10 n)) "beyond trillions")))
+
+(defn is-perfect-square-or-cube?
+  "Returns true if the given number `n` is a perfect square or cube, false otherwise.
+  Uses the given function `f` to calculate the square or cube root.
+
+  Examples:
+  (is-perfect-square-or-cube? 16 math/sqrt) ;; => true
+  (is-perfect-square-or-cube? 27 math/cbrt) ;; => true
+  (is-perfect-square-or-cube? 64 math/sqrt) ;; => true
+  (is-perfect-square-or-cube? 64 math/cbrt) ;; => true
+  (is-perfect-square-or-cube? 9 math/cbrt)  ;; => false
+  "
+  [n f]
+  (let [r (f n)
+        perf? (and (zero? (mod r 1)) (= n (long (apply * (repeat (if (= f math/sqrt) 2 3) r)))))]
+    perf?))
+
+(defn proper-divisors
+  "Returns a sorted sequence of proper divisors from a vector of `prime-factors`.
+
+  Example:
+  (proper-divisors [2 3])  ;; => (1 2 3 6)
+  "
+  [prime-factors]
+  (->> prime-factors
+       (combo/subsets)
+       (map #(apply * %))
+       (sort)))
+
+(defn number-summary
+  "Computes a summary of the arithmetic properties of a natural number `n`, as a data structure.
+
+  Example:
+
+  {:n 16,
+   :spellout-number \"sixteen\",
+   :scale \"tens\",
+   :is-even-or-odd? :even,
+   :prime? false
+   :perfect-square? true,
+   :perfect-cube? false,
+   :divisor-sum 31,
+   :divisor-type :deficient,
+   :divisors (1 2 4 8 16),
+   :prime-factors [2 2 2 2],
+"
+  [n]
+  {:pre [(or (zero? n) (pos-int? n))]}
+  (if (zero? n) zero-arithmetic-properties
+      (let [ret-prime-factors (prime-factors n)
+            divisors (proper-divisors ret-prime-factors)
+            divisor-sum (apply + divisors)
+            divisor-sum-half (/ divisor-sum 2)]
+        {:n n
+         :spellout-number (pp/cl-format nil "~R" n)
+         :scale (get-scale-name n)
+         :divisors divisors
+         :is-even-or-odd? (if (even? n) :even :odd)
+         :divisor-sum divisor-sum
+         :divisor-type (cond
+                         (= n divisor-sum-half) :perfect
+                         (< n divisor-sum-half) :abundant
+                         :else :deficient)
+         :prime-factors ret-prime-factors
+         :prime? (= ret-prime-factors (vector n))
+         :perfect-square? (is-perfect-square-or-cube? n math/sqrt)
+         :perfect-cube? (is-perfect-square-or-cube? n math/cbrt)})))
--- test/dimagid/arithmetic_test.clj
+++ test/dimagid/arithmetic_test.clj
@@ -1,8 +1,6 @@
 ;; ---------------------------------------------------------
 ;; dimagid.arithmetic.-test
 ;;
-;; Example unit tests for dimagid.arithmetic
-;;
 ;; - `deftest` - test a specific function
 ;; - `testing` logically group assertions within a function test
 ;; - `is` assertion:  expected value then function call
@@ -14,14 +12,172 @@
    [clojure.test :refer [deftest is testing]]
    [dimagid.arithmetic :as arithmetic]))
 
+(deftest first-prime
+  (testing "the first prime is 2"
+    (is (= 2 (arithmetic/nth-prime 1)))))
 
-(deftest application-test
-  (testing "TODO: Start with a failing test, make it pass, then refactor"
+(deftest second-prime
+  (testing "the second prime is 3"
+    (is (= 3 (arithmetic/nth-prime 2)))))
 
-    ;; TODO: fix greet function to pass test
-    (is (= "dimagid application developed by the secret engineering team"
-           (arithmetic/greet)))
+(deftest sixth-prime
+  (testing "the sixth prime is 13"
+    (is (= 13 (arithmetic/nth-prime 6)))))
 
-    ;; TODO: fix test by calling greet with {:team-name "Practicalli Engineering"}
-    (is (= (arithmetic/greet "Practicalli Engineering")
-           "dimagid service developed by the Practicalli Engineering team"))))
+(deftest ten-thousand-first-prime
+  (testing "the ten thousand and first prime is 104743"
+    (is (= 104743 (arithmetic/nth-prime 10001)))))
+
+(deftest zeroth-prime
+  (testing "there is no zeroth prime"
+    (is (thrown? AssertionError (arithmetic/nth-prime 0)))))
+
+(deftest negativeth-prime
+  (testing "there is no negativeth prime"
+    (is (thrown? AssertionError (arithmetic/nth-prime -2)))))
+
+(deftest floatth-prime
+  (testing "there is no floatth prime"
+    (is (thrown? AssertionError (arithmetic/nth-prime 3.3)))))
+
+(deftest one
+  (is (= [] (arithmetic/prime-factors 1))))
+
+(deftest two
+  (is (= [2] (arithmetic/prime-factors 2))))
+
+(deftest three
+  (is (= [3] (arithmetic/prime-factors 3))))
+
+(deftest four
+  (is (= [2, 2] (arithmetic/prime-factors 4))))
+
+(deftest six
+  (is (= [2, 3] (arithmetic/prime-factors 6))))
+
+(deftest eight
+  (is (= [2, 2, 2] (arithmetic/prime-factors 8))))
+
+(deftest nine
+  (is (= [3, 3] (arithmetic/prime-factors 9))))
+
+(deftest twenty-seven
+  (is (= [3, 3, 3] (arithmetic/prime-factors 27))))
+
+(deftest six-hundred-twenty-five
+  (is (= [5, 5, 5, 5] (arithmetic/prime-factors 625))))
+
+(deftest a-large-number
+  (is (= [5, 17, 23, 461] (arithmetic/prime-factors 901255))))
+
+(deftest a-huge-number
+  (is (= [11, 9539, 894119] (arithmetic/prime-factors 93819012551))))
+
+(deftest first-n-primes
+  (testing "the first n primes"
+    (is (= [2 3 5 7] (arithmetic/primes-up-to-n 10)))
+    (is (= [2 3 5 7] (arithmetic/primes-up-to-n 7)))
+    (is (= [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331 337 347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499 503 509 521 523 541 547 557 563 569 571 577 587 593 599 601 607 613 617 619 631 641 643 647 653 659 661 673 677 683 691 701 709 719 727 733 739 743 751 757 761 769 773 787 797 809 811 821 823 827 829 839 853 857 859 863 877 881 883 887 907 911 919 929 937 941 947 953 967 971 977 983 991 997] (arithmetic/primes-up-to-n 1000)))))
+
+(deftest edge-cases
+  (testing "edge cases"
+    (is (= [] (arithmetic/primes-up-to-n 1)))
+    (is (= [2] (arithmetic/primes-up-to-n 2)))))
+
+(deftest invalid-inputs
+  (testing "invalid inputs"
+    (is (thrown? AssertionError (arithmetic/primes-up-to-n -1)))
+    (is (thrown? AssertionError (arithmetic/primes-up-to-n -100)))
+    (is (thrown? AssertionError (arithmetic/primes-up-to-n 1.5)))
+    (is (thrown? AssertionError (arithmetic/primes-up-to-n "4")))))
+
+(deftest test-number-summary
+  (testing "Prime Number"
+    (let [n 7
+          result (arithmetic/number-summary n)]
+      (is (= (:n result) n))
+      (is (= (:spellout-number result) "seven"))
+      (is (= (:scale result) "unit"))
+      (is (= (:divisors result) [1 7]))
+      (is (= (:is-even-or-odd? result) :odd))
+      (is (= (:divisor-sum result) 8))
+      (is (= (:divisor-type result) :deficient))
+      (is (= (:prime-factors result) [7]))
+      (is (:prime? result))
+      (is (not (:perfect-square? result)))
+      (is (not (:perfect-cube? result)))))
+
+  (testing "Perfect Number"
+    (let [n 6
+          result (arithmetic/number-summary n)]
+      (is (= (:n result) n))
+      (is (= (:spellout-number result) "six"))
+      (is (= (:scale result) "unit"))
+      (is (= (:divisors result) [1 2 3 6]))
+      (is (= (:is-even-or-odd? result) :even))
+      (is (= (:divisor-sum result) 12))
+      (is (= (:divisor-type result) :perfect))
+      (is (= (:prime-factors result) [2 3]))
+      (is (not (:prime? result)))
+      (is (not (:perfect-square? result)))
+      (is (not (:perfect-cube? result)))))
+
+  (testing "Abundant Number"
+    (let [n 12
+          result (arithmetic/number-summary n)]
+      (is (= (:n result) n))
+      (is (= (:spellout-number result) "twelve"))
+      (is (= (:scale result) "tens"))
+      (is (= (:divisors result) [1 2 3 4 6 12]))
+      (is (= (:is-even-or-odd? result) :even))
+      (is (= (:divisor-sum result) 28))
+      (is (= (:divisor-type result) :abundant))
+      (is (= (:prime-factors result) [2 2 3]))
+      (is (not (:prime? result)))
+      (is (not (:perfect-square? result)))
+      (is (not (:perfect-cube? result)))))
+
+  (testing "Perfect Square"
+    (let [n 16
+          result (arithmetic/number-summary n)]
+      (is (= (:n result) n))
+      (is (= (:spellout-number result) "sixteen"))
+      (is (= (:scale result) "tens"))
+      (is (= (:divisors result) [1 2 4 8 16]))
+      (is (= (:is-even-or-odd? result) :even))
+      (is (= (:divisor-sum result) 31))
+      (is (= (:divisor-type result) :deficient))
+      (is (= (:prime-factors result) [2 2 2 2]))
+      (is (not (:prime? result)))
+      (is (:perfect-square? result))
+      (is (not (:perfect-cube? result)))))
+
+  (testing "Perfect Cube"
+    (let [n 27
+          result (arithmetic/number-summary n)]
+      (is (= (:n result) n))
+      (is (= (:spellout-number result) "twenty-seven"))
+      (is (= (:scale result) "tens"))
+      (is (= (:divisors result) [1 3 9 27]))
+      (is (= (:is-even-or-odd? result) :odd))
+      (is (= (:divisor-sum result) 40))
+      (is (= (:divisor-type result) :deficient))
+      (is (= (:prime-factors result) [3 3 3]))
+      (is (not (:prime? result)))
+      (is (not (:perfect-square? result)))
+      (is (:perfect-cube? result))))
+
+  (testing "Number Summary with Zero"
+    (let [n 0
+          result (arithmetic/number-summary n)]
+      (is (= (:n result) n))
+      (is (= (:spellout-number result) "zero"))
+      (is (= (:scale result) "zero"))
+      (is (= (:divisors result) []))
+      (is (= (:is-even-or-odd? result) :even))
+      (is (= (:divisor-sum result) 0))
+      (is (= (:divisor-type result) nil))
+      (is (= (:prime-factors result) []))
+      (is (not (:prime? result)))
+      (is (not (:perfect-square? result)))
+      (is (not (:perfect-cube? result))))))
